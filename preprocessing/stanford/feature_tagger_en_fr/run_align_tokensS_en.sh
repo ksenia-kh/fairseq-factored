5a52902980ed14fd5a5ca322a14b2d3adf5d99ab
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-09-25 15:34:27 +0200

    align tags with bpe tokens

---
+++ preprocessing/stanford/feature_tagger_en_fr/run_align_tokensS_en.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#SBATCH -p veu # Partition to submit to
+#SBATCH --mem=10G # Memory
+#SBATCH --ignore-pbs
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/align_en_tokens_tags.log
+
+
+FEATURE_TAGGER_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/preprocessing/stanford/feature_tagger_en_fr"
+PYTHON="python"
+
+# Activate conda environment
+source ~/.bashrc
+conda activate myenv
+
+stdbuf -i0 -e0 -o0 $PYTHON $FEATURE_TAGGER_DIR/align_features_en_bpe_tokensS.py
