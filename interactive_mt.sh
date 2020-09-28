3e95a39f6e348bf9a83234becfaef53cd7a57979
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-09-28 17:43:27 +0200

    generate mt translation

--- interactive_mt.sh
+++ interactive_mt.sh
@@ -18,6 +18,10 @@ CP="checkpoint_best.pt"
 PYTHON="python"
 FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
 
+# Activate conda environment
+source ~/.bashrc
+conda activate myenv
+
 #stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/generate.py $DEST_DIR --path $CP_DIR/$CP \
 #    --beam 5 --batch-size 1 --source-lang ${SRC} --target-lang ${TGT} --task translation --remove-bpe
 
