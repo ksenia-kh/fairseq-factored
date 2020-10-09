712e283457177ca7eeccfad8d470fe0658ee8de2
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-10-09 16:00:53 +0200

    factored one sum w pos, baseline 100k upd

--- fairseq/models/factored_transformer_one_encoder_sum.py
+++ fairseq/models/factored_transformer_one_encoder_sum.py
@@ -867,3 +867,18 @@ def factored_one_encoder_iwslt_de_en(args):
     args.decoder_attention_heads = getattr(args, 'decoder_attention_heads', 2)
     args.decoder_layers = getattr(args, 'decoder_layers', 5)
     factored_one_encoder_base_architecture(args)
+
+# French WMT architecture
+
+@register_model_architecture('factored_transformer_one_encoder_sum', 'factored_transformer_one_encoder_sum_wmt_en_fr_posS')
+def transformer_wmt_en_fr(args):
+    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 512)
+    args.encoder_ffn_embed_dim = getattr(args, 'encoder_ffn_embed_dim', 2048)
+    args.encoder_layers = getattr(args, 'encoder_layers', 6)
+    args.encoder_attention_heads = getattr(args, 'encoder_attention_heads', 8)
+    args.encoder_embed_dim_sizes = {'en_tokensS': 512, 'en_posS': 512}
+    args.decoder_embed_dim = getattr(args, 'decoder_embed_dim', 512)
+    args.decoder_ffn_embed_dim = getattr(args, 'decoder_ffn_embed_dim', 2048)
+    args.decoder_attention_heads = getattr(args, 'decoder_attention_heads', 6)
+    args.decoder_layers = getattr(args, 'decoder_layers', 8)
+    factored_one_encoder_base_architecture(args)
