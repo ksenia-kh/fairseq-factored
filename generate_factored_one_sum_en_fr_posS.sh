0b01bfddc3425c67b1241a1e08e47add1ea99c36
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-10-12 16:06:12 +0200

    factored one sum w pos generate

---
+++ generate_factored_one_sum_en_fr_posS.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+#SBATCH -p veu # Partition to submit to
+#SBATCH --gres=gpu:2
+#SBATCH --mem=20G # Memory
+#SBATCH --ignore-pbs                                                            
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/generate-factored-wmt-one-sum-en-fr-pos.log
+
+WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-fr/en-fr-preprocessed-bpe"
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints3"
+CP="checkpoint_best.pt"
+PYTHON="python"
+FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
+
+source ~/.bashrc
+conda activate myenv
+
+mkdir -p $CP_DIR
+
+stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/generate.py $DEST_DIR --path $CP_DIR/$CP \
+	--beam 5 --batch-size 1 --lang-pairs en_tokensS-fr_tokensS,en_pos-fr_tokensS --task factored_translation --remove-bpe --target-lang fr_tokensS --multiple-encoders False
