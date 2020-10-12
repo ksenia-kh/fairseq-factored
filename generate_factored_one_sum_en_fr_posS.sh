45ebcf5630a86034fe5b5b77bd06b747e5820ed0
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-10-12 16:18:04 +0200

    factored one sum w pos generate

--- generate_factored_one_sum_en_fr_posS.sh
+++ generate_factored_one_sum_en_fr_posS.sh
@@ -2,7 +2,7 @@
 
 
 #SBATCH -p veu # Partition to submit to
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --mem=20G # Memory
 #SBATCH --ignore-pbs                                                            
 #SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/generate-factored-wmt-one-sum-en-fr-pos.log
