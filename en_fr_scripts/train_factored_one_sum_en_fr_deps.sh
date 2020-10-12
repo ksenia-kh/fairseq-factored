b0a4b43ef5f5fcae107c79d6559d32491efe3a6a
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-10-12 16:58:57 +0200

    train w deps, average cps, generate baseline w gpu

--- en_fr_scripts/train_factored_one_sum_en_fr_deps.sh
+++ en_fr_scripts/train_factored_one_sum_en_fr_deps.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:2
 #SBATCH --mem=20G # Memory
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/train-factored-wmt-one-sum-en-fr-tags.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/train-factored-wmt-one-sum-en-fr-deps.log
 
 WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-fr/en-fr-preprocessed-bpe"
 CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints5"
