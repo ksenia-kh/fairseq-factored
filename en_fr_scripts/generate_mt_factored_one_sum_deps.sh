c3dec9eb447a5110c781f144b92d286148b390a2
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-10-13 18:15:07 +0200

    preprocess and generate mt

---
+++ en_fr_scripts/generate_mt_factored_one_sum_deps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+#SBATCH -p veu # Partition to submit to
+#SBATCH --gres=gpu:0
+#SBATCH --mem=10G # Memory
+#SBATCH --ignore-pbs                                                            
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/generate-mt.log
+
+
+WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-fr/en-fr-preprocessed-bpe"
+SRC1="en_tokensS"
+SRC2="en_deps"
+TGT="fr_tokensS"
+DEST_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/"
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints5"
+CP="checkpoint_last.pt"
+#CP="model.pt"
+PYTHON="python"
+FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored"
+
+# Activate conda environment
+source ~/.bashrc
+conda activate myenv
+
+stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/generate.py $DEST_DIR --path $CP_DIR/$CP \
+	--beam 5 --batch-size 1 --source-lang ${SRC} --target-lang ${TGT} --task translation --remove-bpe
