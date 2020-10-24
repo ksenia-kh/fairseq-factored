f952819068d4e8c594c615f5c8d3cbfe103c28ae
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-10-24 16:56:23 +0200

    tokens de

---
+++ preprocessing/stanford/feature_tagger_en_fr/run_feature_tagger_spaces_de.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+#SBATCH -p veu # Partition to submit to
+#SBATCH -x veuc09,veuc06
+#SBATCH --gres=gpu:1
+#SBATCH --mem=20G # Memory
+#SBATCH --ignore-pbs
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/feature_tagger_de_spaces.log
+
+
+FEATURE_TAGGER_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/preprocessing/stanford/feature_tagger_en_fr"
+PYTHON="python"
+
+# Activate conda environment
+source ~/.bashrc
+conda activate myenv
+
+stdbuf -i0 -e0 -o0 $PYTHON $FEATURE_TAGGER_DIR/feature_tagger_spaces_de.py
