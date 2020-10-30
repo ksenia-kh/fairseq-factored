e8f1322a6bea73089a66945afca466e5a59b1d8d
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-10-30 16:10:30 +0100

    train baseline de with higher do

--- en_fr_scripts/train_de_en_baseline.sh
+++ en_fr_scripts/train_de_en_baseline.sh
@@ -8,7 +8,7 @@
 #SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/train-baseline-wmt-bpe-de-en.log
 
 WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/de-en/de-en-preprocessed-bpe"
-CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints12-de-b"
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints16-de-b-do0.3"
 PYTHON="python"
 FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
 
@@ -18,4 +18,4 @@ conda activate myenv
 mkdir -p $CP_DIR
 
 stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/train.py $WORKING_DIR \
- --arch  transformer_wmt_en_de --share-all-embeddings  --optimizer adam --adam-betas '(0.9, 0.98)' --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 4000 --lr 0.0001 --min-lr 1e-09 --dropout 0.1 --weight-decay 0.0001 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 4000  --save-dir $CP_DIR --source-lang en_tokensS --target-lang de_tokensS --max-update 100000
+ --arch  transformer_wmt_en_de --share-all-embeddings  --optimizer adam --adam-betas '(0.9, 0.98)' --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 4000 --lr 0.0001 --min-lr 1e-09 --dropout 0.3 --weight-decay 0.0001 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 4000  --save-dir $CP_DIR --source-lang en_tokensS --target-lang de_tokensS --max-update 100000
