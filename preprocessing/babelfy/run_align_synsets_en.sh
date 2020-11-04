f00d073a635c6ae2082cdfb924c5109b30afacf8
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-11-04 16:40:00 +0100

    align and assign lemmas to synsets for fr

---
+++ preprocessing/babelfy/run_align_synsets_en.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#SBATCH -p veu # Partition to submit to
+#SBATCH --mem=8G # Memory
+#SBATCH --gres=gpu:1
+#SBATCH --ignore-pbs                                                            
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/europarl_assign_synsets_en_fr.log
+
+SCRIPT_PATH="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/preprocessing/babelfy/assign_align_synsets_without_at_lemmas.py"
+PYTHON="python"
+
+source ~/.bashrc
+conda activate myenv
+
+stdbuf -i0 -e0 -o0 $PYTHON $SCRIPT_PATH
+#$PYTHON SSCRIPT_PATH > "/home/usuaris/veu/jordi.armengol/tfg/new/logs/get_synsets_de_1.log"
