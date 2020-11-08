0520cfe3e9c07825aa9debecbb027600f0c36a6c
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-11-08 15:10:16 +0100

    generate baseline de with vaswani pars

--- en_fr_scripts/generate_baseline_de_v.sh
+++ en_fr_scripts/generate_baseline_de_v.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=10G # Memory
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/generate-baseline-de-do0.3.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/generate-baseline-de-v.log
 
 
 WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/de-en/de-en-preprocessed-bpe"
