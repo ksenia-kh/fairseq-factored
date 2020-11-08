cc3a6fad13d8fd07addfd751e7561b32a2bd33c6
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-11-08 11:46:40 +0100

    generate baseline de with vaswani pars

--- en_fr_scripts/average_base.sh
+++ en_fr_scripts/average_base.sh
@@ -5,11 +5,11 @@
 #SBATCH --gres=gpu:0
 #SBATCH --mem=10G # Memory
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/average3.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/average17.log
 
 
 WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-fr/en-fr-preprocessed-bpe"
-CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints3"
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints17-de-b-v"
 PYTHON="python"
 FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
 
@@ -18,4 +18,4 @@ conda activate myenv
 
 
 stdbuf -i0 -e0 -o0 $PYTHON  $FAIRSEQ_DIR/scripts/average_checkpoints.py --inputs $CP_DIR \
-	--num-epoch-checkpoints 10 --output $CP_DIR/model.pt
+	--num-epoch-checkpoints 10 --output $CP_DIR/average_model.pt
