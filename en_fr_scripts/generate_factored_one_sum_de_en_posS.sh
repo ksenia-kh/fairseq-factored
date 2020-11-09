eb3d9e52782d366270d786ee29a9018951907b2b
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-11-09 13:43:33 +0100

    generate factored pos de

--- en_fr_scripts/generate_factored_one_sum_de_en_posS.sh
+++ en_fr_scripts/generate_factored_one_sum_de_en_posS.sh
@@ -5,11 +5,11 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=20G # Memory
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/generate-factored-wmt-one-sum-en-de-pos.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/generate-factored-wmt-one-sum-en-de-pos-v.log
 
 WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/de-en/de-en-preprocessed-bpe"
 CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints13-de-pos"
-CP="checkpoint_last.pt"
+CP="average_model.pt"
 PYTHON="python"
 FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
 
