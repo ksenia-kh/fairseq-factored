8ba64a9ae810f79e095855eb232c3074ceb47a04
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-11-10 14:15:57 +0100

    preprocess mt de

--- en_fr_scripts/preprocess_mt.sh
+++ en_fr_scripts/preprocess_mt.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH -p veu # Partition to submit to
-#SBATCH --gres=gpu:0
+#SBATCH --gres=gpu:1
 #SBATCH --mem=10G # Memory
 #SBATCH --ignore-pbs
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/preprocess-joined-bpe-mt-es.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/preprocess-joined-bpe-mt-de.log
 
-WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/mt/en-es/"
+WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/mt/de-en/"
 SRC="en_tokensS"
-TGT="es_tokensS"
+TGT="de_tokensS"
 
 TRN_PREF="corpus.tc"
 VAL_PREF="dev"
@@ -17,8 +17,8 @@ TES_PREF="test"
 PYTHON="python"
 FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
 
-DEST_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-es/en-es-joined-bpe"
-DEST_DIR2="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-es/en-es-preprocessed-bpe"
+DEST_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/de-en/de-en-joined-bpe"
+DEST_DIR2="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/de-en/de-en-preprocessed-bpe"
 
 
 N_OP=32000
@@ -45,5 +45,5 @@ subword-nmt apply-bpe -c ${DEST_DIR}/${TRN_PREF}.codes.${SRC}-${TGT} --vocabular
 #subword-nmt apply-bpe -c ${DEST_DIR}/${TRN_PREF}.codes.${SRC}-${TGT} --vocabulary ${DEST_DIR}/${TRN_PREF}.vocab.${TGT} --vocabulary-threshold 50 < ${WORKING_DIR}/${TES_PREF}.${TGT} > ${DEST_DIR}/${TES_PREF}.bpe.${TGT}
 
 stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang $SRC --target-lang $TGT \
-       --testpref $WORKING_DIR/mt.bpe --destdir $WORKING_DIR --srcdict $DEST_DIR2/dict.en_tokensS.txt --tgtdict $DEST_DIR2/dict.es_tokensS.txt --nwordstgt $N_OP --nwordssrc $N_OP
+       --testpref $WORKING_DIR/mt.bpe --destdir $WORKING_DIR --srcdict $DEST_DIR2/dict.en_tokensS.txt --tgtdict $DEST_DIR2/dict.de_tokensS.txt --nwordstgt $N_OP --nwordssrc $N_OP
        #--trainpref $DEST_DIR/${TRN_PREF}.bpe  #--validpref $DEST_DIR/${VAL_PREF}.bpe \
