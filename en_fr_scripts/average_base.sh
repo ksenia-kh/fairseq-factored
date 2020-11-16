77bfdbd4bb14bad97e07cb2e1e47436ae3b680ec
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-11-16 14:33:48 +0100

    average baseline ru

--- en_fr_scripts/average_base.sh
+++ en_fr_scripts/average_base.sh
@@ -9,7 +9,7 @@
 
 
 WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-fr/en-fr-preprocessed-bpe"
-CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints15-de-deps"
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints20-ru-b"
 PYTHON="python"
 FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
 
@@ -18,4 +18,4 @@ conda activate myenv
 
 
 stdbuf -i0 -e0 -o0 $PYTHON  $FAIRSEQ_DIR/scripts/average_checkpoints.py --inputs $CP_DIR \
-	--num-epoch-checkpoints 5 --output $CP_DIR/average_model.pt
+	--num-epoch-checkpoints 10 --output $CP_DIR/average_model.pt
