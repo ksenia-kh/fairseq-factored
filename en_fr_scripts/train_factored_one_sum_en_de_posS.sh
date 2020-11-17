fe5e16a968189b20d3aac4de083ba1fda83877b6
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-11-17 12:45:25 +0100

    train factored pos de

--- en_fr_scripts/train_factored_one_sum_en_de_posS.sh
+++ en_fr_scripts/train_factored_one_sum_en_de_posS.sh
@@ -21,4 +21,10 @@ mkdir -p $CP_DIR
 # --task factored_translation --arch factored_transformer_iwslt_de_en  --optimizer adam --adam-betas '(0.9, 0.98)' --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 4000 --lr 0.0005 --min-lr 1e-09 --dropout 0.3 --weight-decay 0.0001 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 4000  --save-dir $CP_DIR --lang-pairs de-en,de_postags_at-en --max-update 50000 --factors-to-freeze de_postags_at-en --freeze-factors-epoch 7
 
 stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/train.py $WORKING_DIR \
- --task factored_translation --arch factored_transformer_one_encoder_sum_wmt_en_fr_posS --share-all-embeddings --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 4000 --lr 0.0007 --min-lr 1e-09 --dropout 0.1 --weight-decay 0.0 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 4096  --save-dir $CP_DIR --lang-pairs en_tokensS-de_tokensS,en_pos-de_tokensS --max-update 160000 --multiple-encoders False
+ --task factored_translation --arch factored_transformer_one_encoder_sum_wmt_en_fr_posS \
+ --share-all-embeddings --optimizer adam --adam-betas '(0.9, 0.98)' \
+ --clip-norm 0.0 --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 \
+ --warmup-updates 4000 --lr 0.0007 --min-lr 1e-09 --dropout 0.1 --weight-decay 0.0 \
+ --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 4096 \
+  --save-dir $CP_DIR --lang-pairs en_tokensS-de_tokensS,en_pos-de_tokensS --max-update 320000 \
+  --multiple-encoders False
