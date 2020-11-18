27947aae6fd2dd2ce4dd3c7b1966490ffb52dad9
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-11-18 11:24:34 +0100

    generate factored pos ru

--- en_fr_scripts/average_base.sh
+++ en_fr_scripts/average_base.sh
@@ -5,11 +5,11 @@
 #SBATCH --gres=gpu:0
 #SBATCH --mem=10G # Memory
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/average16.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/average21.log
 
 
 WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-fr/en-fr-preprocessed-bpe"
-CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints16-de-lemmas"
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints21-ru-pos"
 PYTHON="python"
 FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
 
@@ -18,4 +18,4 @@ conda activate myenv
 
 
 stdbuf -i0 -e0 -o0 $PYTHON  $FAIRSEQ_DIR/scripts/average_checkpoints.py --inputs $CP_DIR \
-	--num-epoch-checkpoints 5 --output $CP_DIR/average_model.pt
+	--num-epoch-checkpoints 10 --output $CP_DIR/average_model.pt
