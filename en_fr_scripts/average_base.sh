e81ed6addc8b8430d1ab464e95dd965ff010a1e5
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-11-19 18:15:48 +0100

    average 24

--- en_fr_scripts/average_base.sh
+++ en_fr_scripts/average_base.sh
@@ -5,11 +5,11 @@
 #SBATCH --gres=gpu:0
 #SBATCH --mem=10G # Memory
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/average15.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/average24.log
 
 
 WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-ru/en-ru-preprocessed-bpe"
-CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints15-de-deps"
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints24-ru-lemmas"
 PYTHON="python"
 FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
 
@@ -18,4 +18,4 @@ conda activate myenv
 
 
 stdbuf -i0 -e0 -o0 $PYTHON  $FAIRSEQ_DIR/scripts/average_checkpoints.py --inputs $CP_DIR \
-	--num-epoch-checkpoints 5 --output $CP_DIR/average_model.pt
+	--num-epoch-checkpoints 10 --output $CP_DIR/average_model.pt
