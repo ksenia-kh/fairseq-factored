95ad990a8388242861aa5c2c5fd85a4fb37c378f
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-11-19 13:25:36 +0100

    generate factored lemmas ru

---
+++ en_fr_scripts/generate_factored_one_sum_en_ru_lemmas.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+#SBATCH -p veu # Partition to submit to
+#SBATCH --gres=gpu:1
+#SBATCH --mem=20G # Memory
+#SBATCH --ignore-pbs                                                            
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/generate-factored-wmt-one-sum-en-ru-lemmas.log
+
+WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-ru/en-ru-preprocessed-bpe"
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints24-ru-lemmas"
+CP="average_model.pt"
+PYTHON="python"
+FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
+
+source ~/.bashrc
+conda activate myenv
+
+#mkdir -p $CP_DIR
+
+stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/generate.py $WORKING_DIR --path $CP_DIR/$CP \
+	--beam 5 --batch-size 1 --lang-pairs en_tokensS-ru_tokensS,en_lemmas-ru_tokensS --task factored_translation --remove-bpe --target-lang ru_tokensS --multiple-encoders False
