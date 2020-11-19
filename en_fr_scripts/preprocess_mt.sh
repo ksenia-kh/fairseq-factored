4a68b3ddd5c895e456040676de810523b143edbd
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-11-19 12:40:28 +0100

    preprocess ru mt

--- en_fr_scripts/preprocess_mt.sh
+++ en_fr_scripts/preprocess_mt.sh
@@ -4,11 +4,11 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=10G # Memory
 #SBATCH --ignore-pbs
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/preprocess-joined-bpe-mt-de.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/preprocess-joined-bpe-mt-ru.log
 
-WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/mt/de-en/"
+WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/mt/en-ru/"
 SRC="en_tokensS"
-TGT="de_tokensS"
+TGT="ru_tokensS"
 
 TRN_PREF="corpus.tc"
 VAL_PREF="dev"
@@ -17,8 +17,8 @@ TES_PREF="test"
 PYTHON="python"
 FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
 
-DEST_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/de-en/de-en-joined-bpe"
-DEST_DIR2="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/de-en/de-en-preprocessed-bpe"
+DEST_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-ru/en-ru-joined-bpe"
+DEST_DIR2="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-ru/en-ru-preprocessed-bpe"
 
 
 N_OP=32000
@@ -45,5 +45,5 @@ subword-nmt apply-bpe -c ${DEST_DIR}/${TRN_PREF}.codes.${SRC}-${TGT} --vocabular
 #subword-nmt apply-bpe -c ${DEST_DIR}/${TRN_PREF}.codes.${SRC}-${TGT} --vocabulary ${DEST_DIR}/${TRN_PREF}.vocab.${TGT} --vocabulary-threshold 50 < ${WORKING_DIR}/${TES_PREF}.${TGT} > ${DEST_DIR}/${TES_PREF}.bpe.${TGT}
 
 stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang $SRC --target-lang $TGT \
-       --testpref $WORKING_DIR/mt.bpe --destdir $WORKING_DIR --srcdict $DEST_DIR2/dict.en_tokensS.txt --tgtdict $DEST_DIR2/dict.de_tokensS.txt --nwordstgt $N_OP --nwordssrc $N_OP
+       --testpref $WORKING_DIR/mt.bpe --destdir $WORKING_DIR --srcdict $DEST_DIR2/dict.en_tokensS.txt --tgtdict $DEST_DIR2/dict.ru_tokensS.txt --nwordstgt $N_OP --nwordssrc $N_OP
        #--trainpref $DEST_DIR/${TRN_PREF}.bpe  #--validpref $DEST_DIR/${VAL_PREF}.bpe \
