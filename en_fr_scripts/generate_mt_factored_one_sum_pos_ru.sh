309e58a109b5118cca49cc5161ad5fe9bb34ff4b
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-11-19 18:08:26 +0100

    generate mt ru

---
+++ en_fr_scripts/generate_mt_factored_one_sum_pos_ru.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+#SBATCH -p veu # Partition to submit to
+#SBATCH --gres=gpu:1
+#SBATCH --mem=10G # Memory
+#SBATCH --ignore-pbs                                                            
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/generate-mt-factored-one-sum-pos-ru.log
+
+
+WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-ru/en-ru-preprocessed-bpe"
+SRC1="en_tokensS"
+SRC2="en_pos"
+TGT="ru_tokensS"
+DEST_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/mt/en-ru/"
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints21-ru-pos"
+CP="average_model.pt"
+#CP="model.pt"
+PYTHON="python"
+FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored"
+
+# Activate conda environment
+source ~/.bashrc
+conda activate myenv
+
+#stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/generate.py $DEST_DIR --path $CP_DIR/$CP \
+#	--beam 5 --batch-size 1 --source-lang ${SRC} --target-lang ${TGT} --task translation --remove-bpe
+
+stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/generate.py $DEST_DIR --path $CP_DIR/$CP \
+	--beam 5 --batch-size 1 --lang-pairs $SRC1-$TGT,$SRC2-$TGT --task factored_translation --remove-bpe --target-lang ru_tokensS --multiple-encoders False
