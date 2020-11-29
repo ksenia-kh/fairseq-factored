f39cfe646b1814387f31bf2613089a33d63b81fc
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-11-29 15:18:43 +0100

    HLE project translate mt es

--- preprocessing/stanford/feature_tagger_en_fr/hf_translate_mt.py
+++ preprocessing/stanford/feature_tagger_en_fr/hf_translate_mt.py
@@ -18,12 +18,10 @@ def main():
 	en_original = [line.split('\t')[2] for line in text]
 	n = len(en_original)
 	en_original_to_translate = [LANG+' '+line for line in en_original]
-	print('Generating translations for 1st half')
-	translated1 = model.generate(**tokenizer.prepare_seq2seq_batch(en_original_to_translate[:n//2]))
-	print('Generating translations for 2nd half')
-	translated2 = model.generate(**tokenizer.prepare_seq2seq_batch(en_original_to_translate[n//2:]))
+	print('Generating translations')
+	translated = model.generate(**tokenizer.prepare_seq2seq_batch(en_original_to_translate))
 	print('Decoding output')
-	tgt_text = [tokenizer.decode(t, skip_special_tokens=True) for t in translated1+translated2]
+	tgt_text = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
 	result = [en_original[i]+' ||| '+tgt_text[i]+'\n' for i in range(len(en_original))]
 	with open(os.path.join(PATH, 'en-es.txt'),'w', encoding="utf8") as f:
 		for line in result:
