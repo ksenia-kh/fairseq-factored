b970ce0174e89eba5029636e1e51e07f4e2df3a5
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-11-29 15:44:38 +0100

    HLE project generate mt es

--- preprocessing/stanford/feature_tagger_en_fr/hf_translate_mt.py
+++ preprocessing/stanford/feature_tagger_en_fr/hf_translate_mt.py
@@ -18,8 +18,11 @@ def main():
 	en_original = [line.split('\t')[2] for line in text]
 	n = len(en_original)
 	en_original_to_translate = [LANG+' '+line for line in en_original]
-	print('Generating translations')
-	translated = model.generate(**tokenizer.prepare_seq2seq_batch(en_original_to_translate))
+	print('Generating translations 1')
+	translated1 = model.generate(**tokenizer.prepare_seq2seq_batch(en_original_to_translate[:n//2]))
+	print('Generating translations 2')
+	translated2 = model.generate(**tokenizer.prepare_seq2seq_batch(en_original_to_translate[n//2:]))
+	translated = torch.cat((translated1, translated2), dim=0)
 	print('Decoding output')
 	tgt_text = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
 	result = [en_original[i]+' ||| '+tgt_text[i]+'\n' for i in range(len(en_original))]
