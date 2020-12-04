738bfcfc4f2809be7b399bd4124a2c976d77b39d
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-12-04 13:19:56 +0100

    generate factored es syn and syn pos

--- en_fr_scripts/generate_factored_one_sum_en_es_synsets_pos.sh
+++ en_fr_scripts/generate_factored_one_sum_en_es_synsets_pos.sh
@@ -5,10 +5,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=20G # Memory
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/generate-factored-wmt-one-sum-en-es-synsets-last.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/generate-factored-wmt-one-sum-en-es-synsets-pos.log
 
 WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-es/en-es-preprocessed-bpe"
-CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints12-es-syn"
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints25-es-syn-pos"
 CP="checkpoint_last.pt"
 PYTHON="python"
 FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
@@ -19,4 +19,4 @@ conda activate myenv
 #mkdir -p $CP_DIR
 
 stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/generate.py $WORKING_DIR --path $CP_DIR/$CP \
-	--beam 5 --batch-size 1 --lang-pairs en_tokensS-es_tokensS,en_synsets_wo_at_lemmas-es_tokensS --task factored_translation --remove-bpe --target-lang es_tokensS --multiple-encoders False
+	--beam 5 --batch-size 1 --lang-pairs en_tokensS-es_tokensS,en_synsets_wo_at_pos-es_tokensS --task factored_translation --remove-bpe --target-lang es_tokensS --multiple-encoders False
