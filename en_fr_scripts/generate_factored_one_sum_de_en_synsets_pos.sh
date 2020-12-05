5d08070e8bf938f4f6039a98d1e0c3cef71d3bcd
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-12-05 14:52:51 +0100

    generate factored syn pos de

---
+++ en_fr_scripts/generate_factored_one_sum_de_en_synsets_pos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+#SBATCH -p veu # Partition to submit to
+#SBATCH --gres=gpu:1
+#SBATCH --mem=20G # Memory
+#SBATCH --ignore-pbs                                                            
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/generate-factored-wmt-one-sum-en-de-synsets-pos.log
+
+WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/de-en/de-en-preprocessed-bpe"
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints27-de-syn-pos"
+CP="average_model.pt"
+PYTHON="python"
+FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
+
+source ~/.bashrc
+conda activate myenv
+
+#mkdir -p $CP_DIR
+
+stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/generate.py $WORKING_DIR --path $CP_DIR/$CP \
+	--beam 5 --batch-size 1 --lang-pairs en_tokensS-de_tokensS,en_synsets_wo_at_pos-de_tokensS --task factored_translation --remove-bpe --target-lang de_tokensS --multiple-encoders False
