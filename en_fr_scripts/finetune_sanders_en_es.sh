43634c4f19525bf8d8b2b59f1cc0d78171389826
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-12-29 15:47:28 +0100

    fine tune sanders hd

--- en_fr_scripts/finetune_sanders_en_es.sh
+++ en_fr_scripts/finetune_sanders_en_es.sh
@@ -10,17 +10,17 @@
 WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/sanders-gender-debias/handcraft"
 CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints7-es-ft-s-hd"
 PYTHON="python"
-FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
+FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq/"
 
 source ~/.bashrc
 conda activate myenv
 
 mkdir -p $CP_DIR
 
-stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/train.py $WORKING_DIR \
+stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/fairseq_cli/train.py $WORKING_DIR \
  --arch  transformer_wmt_en_de --share-all-embeddings  --optimizer adam \
  --adam-betas '(0.9, 0.98)' --lr-scheduler inverse_sqrt --scoring bleu --warmup-init-lr 1e-07 \
  --warmup-updates 0 --lr 0.0001 --min-lr 1e-09 --dropout 0.1 --weight-decay 0.0001 \
  --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 4000 \
-  --restore-file $CP_DIR/checkpoint_last.pt --reset-dataloader --reset-lr-scheduler --reset-meters --save-dir $CP_DIR \
+  --finetune-from-model $CP_DIR/checkpoint_last.pt --save-dir $CP_DIR --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
   --source-lang en_tokensS --target-lang es_tokensS --max-update 2000
