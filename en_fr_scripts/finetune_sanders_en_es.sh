bce8d1a64713bc767948ec52c4de05f78ac071ab
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2020-12-29 15:20:48 +0100

    fine tune sanders hd

--- en_fr_scripts/finetune_sanders_en_es.sh
+++ en_fr_scripts/finetune_sanders_en_es.sh
@@ -23,4 +23,4 @@ stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/train.py $WORKING_DIR \
  --warmup-updates 0 --lr 0.0001 --min-lr 1e-09 --dropout 0.1 --weight-decay 0.0001 \
  --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 4000 \
   --finetune-from-model $CP_DIR/checkpoint_last.pt --save-dir $CP_DIR --best-checkpoint-metric bleu --maximize-best-checkpoint-metric\
-  --source-lang en_tokensS --target-lang es_tokensS --max-update 2000
+  --source-lang en_tokensS --target-lang es_tokensS --max-update 4000
