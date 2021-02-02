d2d015bf8f5393936082eebaa88b9558890f4519
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2021-02-02 16:24:16 +0100

    assign synsets lemmas en fr new

--- preprocessing/babelfy/run_align_synsets_en.sh
+++ preprocessing/babelfy/run_align_synsets_en.sh
@@ -5,9 +5,9 @@
 #SBATCH --mem=30G # Memory
 #SBATCH --gres=gpu:1
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/europarl_assign_synsets_en_ru_pos.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/europarl_assign_synsets_en_fr_new.log
 
-SCRIPT_PATH="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/preprocessing/babelfy/assign_align_synsets_without_at_pos.py"
+SCRIPT_PATH="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/preprocessing/babelfy/assign_align_synsets_without_at_lemmas.py"
 PYTHON="python"
 
 source ~/.bashrc
