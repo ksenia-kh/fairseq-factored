1a8728d7857f231a1f8485d8a6d908392f91614d
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2021-02-03 12:31:35 +0100

    train baseline fr new

--- en_fr_scripts/train_en_fr_baseline.sh
+++ en_fr_scripts/train_en_fr_baseline.sh
@@ -5,10 +5,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=20G # Memory
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/train-baseline-wmt-bpe-en-fr.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/train-baseline-wmt-bpe-en-fr-new.log
 
 WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-fr/en-fr-preprocessed-bpe"
-CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints2"
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints33-fr"
 PYTHON="python"
 FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
 
@@ -18,4 +18,9 @@ conda activate myenv
 mkdir -p $CP_DIR
 
 stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/train.py $WORKING_DIR \
- --arch  transformer_wmt_en_de --share-all-embeddings  --optimizer adam --adam-betas '(0.9, 0.98)' --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 4000 --lr 0.0001 --min-lr 1e-09 --dropout 0.1 --weight-decay 0.0001 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 4000  --save-dir $CP_DIR --source-lang en_tokensS --target-lang fr_tokensS --max-update 100000
+ --arch  transformer_wmt_en_de --share-all-embeddings  --optimizer adam --adam-betas '(0.9, 0.98)' \
+ --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 4000 \
+ --lr 0.0001 --min-lr 1e-09 --dropout 0.1 --weight-decay 0.0001 --criterion label_smoothed_cross_entropy \
+ --label-smoothing 0.1 --max-tokens 4096  \
+ --save-dir $CP_DIR --source-lang en_tokensS --target-lang fr_tokensS \
+ --max-update 100000
