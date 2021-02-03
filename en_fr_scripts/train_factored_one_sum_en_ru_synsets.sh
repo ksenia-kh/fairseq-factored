a26a2267e156af94d8ca20d8722c550d0cc26373
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2021-02-03 14:22:50 +0100

    train ru and fr

--- en_fr_scripts/train_factored_one_sum_en_ru_synsets.sh
+++ en_fr_scripts/train_factored_one_sum_en_ru_synsets.sh
@@ -5,10 +5,10 @@
 #SBATCH --gres=gpu:2
 #SBATCH --mem=20G # Memory
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/train-factored-wmt-one-sum-en-fr-synsets-new.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/train-factored-wmt-one-sum-en-ru-synsets.log
 
-WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-fr/en-fr-preprocessed-bpe"
-CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints38-fr-syn"
+WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-ru/en-ru-preprocessed-bpe"
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints40-ru-syn"
 PYTHON="python"
 FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
 
@@ -23,8 +23,8 @@ mkdir -p $CP_DIR
 stdbuf -i0 -e0 -o0 $PYTHON $FAIRSEQ_DIR/train.py $WORKING_DIR \
  --task factored_translation --arch factored_transformer_one_encoder_sum_wmt_en_fr_synsets \
  --share-all-embeddings --optimizer adam --adam-betas '(0.9, 0.98)' \
- --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 4000 \
- --lr 0.0001 --min-lr 1e-09 --dropout 0.1 --weight-decay 0.0001 \
- --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
- --max-tokens 4096  --save-dir $CP_DIR --lang-pairs en_tokensS-fr_tokensS,en_synsets_wo_at_lemmas-fr_tokensS \
- --max-update 100000 --multiple-encoders False
+ --clip-norm 0.0 --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 4000 \
+ --lr 0.0007 --min-lr 1e-09 --dropout 0.1 --weight-decay 0.0 \
+ --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 4096 \
+  --save-dir $CP_DIR --lang-pairs en_tokensS-ru_tokensS,en_synsets_wo_at_lemmas-ru_tokensS \
+  --max-update 160000 --multiple-encoders False
