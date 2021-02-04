926e2ab4385b3e2b340493f485423d570b488d95
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2021-02-04 16:03:07 +0100

    preprocess tags mt fr

--- en_fr_scripts/preprocess_tags_en_fr_mt.sh
+++ en_fr_scripts/preprocess_tags_en_fr_mt.sh
@@ -5,10 +5,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=10G # Memory
 #SBATCH --ignore-pbs
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/preprocess-joined-bpe-tags-ru-mt.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/preprocess-joined-bpe-tags-fr-mt-new.log
 
 
-WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/mt/en-ru/"
+WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/mt/en-fr/"
 
 TRN_PREF="corpus.tc"
 VAL_PREF="dev"
@@ -17,14 +17,14 @@ PYTHON="python"
 
 FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
 
-DEST_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-ru/en-ru-preprocessed-bpe"
+DEST_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-fr/en-fr-preprocessed-bpe"
 
 # Activate conda environment
 source ~/.bashrc
 conda activate myenv
 
 SRC="en_deps"
-TGT="ru_tokensS"
+TGT="fr_tokensS"
 
 #stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang $SRC --target-lang $TGT \
 #    --trainpref $WORKING_DIR/${TRN_PREF}.bpe --validpref $WORKING_DIR/${VAL_PREF}.bpe --testpref $WORKING_DIR/${TES_PREF}.bpe \
@@ -34,7 +34,7 @@ stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang $SRC --targ
        --testpref $WORKING_DIR/mt.bpe --destdir $WORKING_DIR --srcdict $DEST_DIR/dict.${SRC}.txt --tgtdict $DEST_DIR/dict.${TGT}.txt
 
 SRC="en_lemmas"
-TGT="ru_tokensS"
+TGT="fr_tokensS"
 
 #stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang $SRC --target-lang $TGT \
 #    --trainpref $WORKING_DIR/${TRN_PREF}.bpe --validpref $WORKING_DIR/${VAL_PREF}.bpe --testpref $WORKING_DIR/${TES_PREF}.bpe \
@@ -44,7 +44,7 @@ stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang $SRC --targ
        --testpref $WORKING_DIR/mt.bpe --destdir $WORKING_DIR --srcdict $DEST_DIR/dict.${SRC}.txt --tgtdict $DEST_DIR/dict.${TGT}.txt
 
 SRC="en_pos"
-TGT="ru_tokensS"
+TGT="fr_tokensS"
 
 #stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang $SRC --target-lang $TGT \
 #    --trainpref $WORKING_DIR/${TRN_PREF}.bpe --validpref $WORKING_DIR/${VAL_PREF}.bpe --testpref $WORKING_DIR/${TES_PREF}.bpe \
@@ -54,7 +54,7 @@ stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang $SRC --targ
        --testpref $WORKING_DIR/mt.bpe --destdir $WORKING_DIR --srcdict $DEST_DIR/dict.${SRC}.txt --tgtdict $DEST_DIR/dict.${TGT}.txt
 
 SRC="en_tags"
-TGT="ru_tokensS"
+TGT="fr_tokensS"
 
 #stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang $SRC --target-lang $TGT \
 #    --trainpref $WORKING_DIR/${TRN_PREF}.bpe --validpref $WORKING_DIR/${VAL_PREF}.bpe --testpref $WORKING_DIR/${TES_PREF}.bpe \
