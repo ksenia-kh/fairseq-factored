af030d3f47186da268354031d36863f37f20d179
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2021-02-07 14:32:59 +0100

    retrain ru

--- en_fr_scripts/train_factored_one_sum_en_ru_lemmas.sh
+++ en_fr_scripts/train_factored_one_sum_en_ru_lemmas.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=20G # Memory
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/train-factored-wmt-one-sum-en-ru-lemmas.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/train-factored-wmt-one-sum-en-ru-lemmas-new.log
 
 WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-ru/en-ru-preprocessed-bpe"
 CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints24-ru-lemmas"
