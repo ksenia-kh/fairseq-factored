abc50a74cc2570995e8f7193c4e1a8b9a97c0697
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2021-03-02 22:35:20 +0100

    assign europarl correct nchars 4000

--- preprocessing/babelfy/run_align_synsets_en.sh
+++ preprocessing/babelfy/run_align_synsets_en.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=30G # Memory
 #SBATCH --gres=gpu:1
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/europarl_assign_synsets_lemmas_en_fr_new.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/europarl_assign_synsets_lemmas_en_es_new.log
 
 SCRIPT_PATH="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/preprocessing/babelfy/assign_align_synsets_without_at_lemmas.py"
 PYTHON="python"
