ce66bc2096498dd8635e71c92e1d8cf71206ad5a
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2021-03-06 16:22:55 +0100

    preprocess synsets pos ru

--- en_fr_scripts/preprocess_synsets_en_fr.sh
+++ en_fr_scripts/preprocess_synsets_en_fr.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=10G # Memory
 #SBATCH --ignore-pbs
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/preprocess-joined-bpe-synsets-lemmas-ru-new.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/preprocess-joined-bpe-synsets-pos-ru-new.log
 
 
 WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-ru/en-ru-joined-bpe"
@@ -23,7 +23,7 @@ DEST_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-ru/en-ru-pre
 source ~/.bashrc
 conda activate myenv
 
-SRC="en_synsets_wo_at_lemmas"
+SRC="en_synsets_wo_at_pos"
 TGT="ru_tokensS"
 
 stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang $SRC --target-lang $TGT \
