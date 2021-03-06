f541dc5ec9a450f8fae0203d04b2b849387c92a0
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2021-03-06 10:59:42 +0100

    preprocess synsets es lemmas new

--- en_fr_scripts/preprocess_synsets_en_fr.sh
+++ en_fr_scripts/preprocess_synsets_en_fr.sh
@@ -5,10 +5,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=10G # Memory
 #SBATCH --ignore-pbs
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/preprocess-joined-bpe-synsets-pos-fr-new.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/preprocess-joined-bpe-synsets-lemmas-es-new.log
 
 
-WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-fr/en-fr-joined-bpe"
+WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-es/en-es-joined-bpe"
 
 TRN_PREF="corpus.tc"
 VAL_PREF="dev"
@@ -17,15 +17,15 @@ PYTHON="python"
 
 FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/"
 
-DEST_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-fr/en-fr-preprocessed-bpe"
+DEST_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-es/en-es-preprocessed-bpe"
 
 # Activate conda environment
 source ~/.bashrc
 conda activate myenv
 
-SRC="en_synsets_wo_at_pos"
-TGT="fr_tokensS"
+SRC="en_synsets_wo_at_lemmas"
+TGT="es_tokensS"
 
 stdbuf -i0 -e0 -o0  $PYTHON $FAIRSEQ_DIR/preprocess.py --source-lang $SRC --target-lang $TGT \
     --trainpref $WORKING_DIR/${TRN_PREF}.bpe --validpref $WORKING_DIR/${VAL_PREF}.bpe --testpref $WORKING_DIR/${TES_PREF}.bpe \
-    --destdir $DEST_DIR  --tgtdict $DEST_DIR/dict.fr_tokensS.txt --thresholdsrc 1
+    --destdir $DEST_DIR  --tgtdict $DEST_DIR/dict.es_tokensS.txt --thresholdsrc 1
