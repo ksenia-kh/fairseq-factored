d70222b6f33b4573f3fc55e6dd1ccd9ae7d78da3
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2021-03-10 15:32:12 +0100

    encode all fr

--- encode_fr.sh
+++ encode_fr.sh
@@ -2,7 +2,7 @@
 
 
 #SBATCH -p veu # Partition to submit to
-#SBATCH --gres=gpu:0
+#SBATCH --gres=gpu:1
 #SBATCH --mem=20G # Memory
 #SBATCH --ignore-pbs                                                            
 #SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/encode-fr.log
