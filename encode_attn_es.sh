d7dcb93b8d3bca79077251d9356cf4333e65fa41
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2021-04-05 12:45:35 +0200

    encode attention bash es, ru

---
+++ encode_attn_es.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+
+#SBATCH -p veu # Partition to submit to
+#SBATCH --gres=gpu:1
+#SBATCH --mem=20G # Memory
+#SBATCH --ignore-pbs                                                            
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/encode-attn-es.log
+
+WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-es/en-es-preprocessed-bpe"
+CP="checkpoint_last.pt"
+PYTHON="python"
+FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored"
+OUTPUT_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/mt/encodings_attn/es"
+DEST_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/mt/en-es/"
+N=100
+#mkdir tmp
+
+# Activate conda environment
+source ~/.bashrc
+conda activate myenv
+git checkout attention-analysis
+
+
+mkdir -p  $OUTPUT_DIR
+
+: '
+#Bilingual es baseline
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints7-es-b"
+OUTPUT="extracted-attn-enes-b.pkl"
+SRC="en_tokensS"
+TGT="es_tokensS"
+
+cuda_visible_devices="" stdbuf -i0 -e0 -o0 python $FAIRSEQ_DIR/encode_attention.py $DEST_DIR --path $CP_DIR/$CP --n-points $N \
+         --batch-size 1 --source-lang ${SRC} --target-lang ${TGT} --task translation \
+         --output-file $OUTPUT_DIR/$OUTPUT --remove-bpe
+'
+
+# Factored es POS
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints8-es-pos"
+OUTPUT="extracted-attn-enes-f-pos.pkl"
+SRC1="en_tokensS"
+SRC2="en_pos"
+TGT="es_tokensS"
+
+cuda_visible_devices="" stdbuf -i0 -e0 -o0 python $FAIRSEQ_DIR/encode_attention.py $DEST_DIR --path $CP_DIR/$CP --n-points $N \
+         --batch-size 1 --lang-pairs ${SRC1}-${TGT},${SRC2}-${TGT} --source-lang ${SRC1} --target-lang ${TGT} --task factored_translation \
+        --output-file $OUTPUT_DIR/$OUTPUT --remove-bpe --multiple-encoders False
+
+# Factored es TAGS
+
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints9-es-tags"
+OUTPUT="extracted-attn-enes-f-tags.pkl"
+SRC1="en_tokensS"
+SRC2="en_tags"
+TGT="es_tokensS"
+
+cuda_visible_devices="" stdbuf -i0 -e0 -o0 python $FAIRSEQ_DIR/encode_attention.py $DEST_DIR --path $CP_DIR/$CP --n-points $N \
+         --batch-size 1 --lang-pairs ${SRC1}-${TGT},${SRC2}-${TGT} --source-lang ${SRC1} --target-lang ${TGT} --task factored_translation \
+        --output-file $OUTPUT_DIR/$OUTPUT --remove-bpe --multiple-encoders False
+
+
+# Factored es DEPS
+
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints10-es-deps"
+OUTPUT="extracted-attn-enes-f-deps.pkl"
+SRC1="en_tokensS"
+SRC2="en_deps"
+TGT="es_tokensS"
+
+cuda_visible_devices="" stdbuf -i0 -e0 -o0 python $FAIRSEQ_DIR/encode_attention.py $DEST_DIR --path $CP_DIR/$CP --n-points $N \
+         --batch-size 1 --lang-pairs ${SRC1}-${TGT},${SRC2}-${TGT} --source-lang ${SRC1} --target-lang ${TGT} --task factored_translation \
+        --output-file $OUTPUT_DIR/$OUTPUT --remove-bpe --multiple-encoders False
+
+
+# Factored es Lemmas
+
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints11-es-l"
+OUTPUT="extracted-attn-enes-f-lemmas.pkl"
+SRC1="en_tokensS"
+SRC2="en_lemmas"
+TGT="es_tokensS"
+
+cuda_visible_devices="" stdbuf -i0 -e0 -o0 python $FAIRSEQ_DIR/encode_attention.py $DEST_DIR --path $CP_DIR/$CP --n-points $N \
+         --batch-size 1 --lang-pairs ${SRC1}-${TGT},${SRC2}-${TGT} --source-lang ${SRC1} --target-lang ${TGT} --task factored_translation \
+        --output-file $OUTPUT_DIR/$OUTPUT --remove-bpe --multiple-encoders False
+
+
+# Factored es Syn-Lemmas
+
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints12-es-syn"
+OUTPUT="extracted-attn-enes-f-syn-lemmas.pkl"
+SRC1="en_tokensS"
+SRC2="en_synsets_wo_at_lemmas"
+TGT="es_tokensS"
+
+cuda_visible_devices="" stdbuf -i0 -e0 -o0 python $FAIRSEQ_DIR/encode_attention.py $DEST_DIR --path $CP_DIR/$CP --n-points $N \
+         --batch-size 1 --lang-pairs ${SRC1}-${TGT},${SRC2}-${TGT} --source-lang ${SRC1} --target-lang ${TGT} --task factored_translation \
+        --output-file $OUTPUT_DIR/$OUTPUT --remove-bpe --multiple-encoders False
+
+
+# Factored es Syn-Pos
+
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints25-es-syn-pos"
+OUTPUT="extracted-attn-enes-f-syn-pos.pkl"
+SRC1="en_tokensS"
+SRC2="en_synsets_wo_at_pos"
+TGT="es_tokensS"
+
+cuda_visible_devices="" stdbuf -i0 -e0 -o0 python $FAIRSEQ_DIR/encode_attention.py $DEST_DIR --path $CP_DIR/$CP --n-points $N \
+         --batch-size 1 --lang-pairs ${SRC1}-${TGT},${SRC2}-${TGT} --source-lang ${SRC1} --target-lang ${TGT} --task factored_translation \
+        --output-file $OUTPUT_DIR/$OUTPUT --remove-bpe --multiple-encoders False
+
+
---
+++ encode_attn_ru.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+
+#SBATCH -p veu # Partition to submit to
+#SBATCH --gres=gpu:1
+#SBATCH --mem=20G # Memory
+#SBATCH --ignore-pbs                                                            
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/encode-attn-ru.log
+
+WORKING_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/europarl/en-ru/en-ru-preprocessed-bpe"
+CP="average_model.pt"
+PYTHON="python"
+FAIRSEQ_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored"
+OUTPUT_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/mt/encodings_attn/ru"
+DEST_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/data/mt/en-ru/"
+N=100
+#mkdir tmp
+
+# Activate conda environment
+source ~/.bashrc
+conda activate myenv
+git checkout attention-analysis
+
+mkdir -p  $OUTPUT_DIR
+
+: '
+#Bilingual ru baseline
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints20-ru-b"
+OUTPUT="extracted-attn-enru-b.pkl"
+SRC="en_tokensS"
+TGT="ru_tokensS"
+
+cuda_visible_devices="" stdbuf -i0 -e0 -o0 python $FAIRSEQ_DIR/encode_attention.py $DEST_DIR --path $CP_DIR/$CP --n-points $N \
+         --batch-size 1 --source-lang ${SRC} --target-lang ${TGT} --task translation \
+         --output-file $OUTPUT_DIR/$OUTPUT --remove-bpe
+'
+
+# Factored ru POS
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints21-ru-pos"
+OUTPUT="extracted-attn-enru-f-pos.pkl"
+SRC1="en_tokensS"
+SRC2="en_pos"
+TGT="ru_tokensS"
+
+cuda_visible_devices="" stdbuf -i0 -e0 -o0 python $FAIRSEQ_DIR/encode_attention.py $DEST_DIR --path $CP_DIR/$CP --n-points $N \
+         --batch-size 1 --lang-pairs ${SRC1}-${TGT},${SRC2}-${TGT} --source-lang ${SRC1} --target-lang ${TGT} --task factored_translation \
+        --output-file $OUTPUT_DIR/$OUTPUT --remove-bpe --multiple-encoders False
+
+# Factored ru TAGS
+
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints22-ru-tags"
+OUTPUT="extracted-attn-enru-f-tags.pkl"
+SRC1="en_tokensS"
+SRC2="en_tags"
+TGT="ru_tokensS"
+
+cuda_visible_devices="" stdbuf -i0 -e0 -o0 python $FAIRSEQ_DIR/encode_attention.py $DEST_DIR --path $CP_DIR/$CP --n-points $N \
+         --batch-size 1 --lang-pairs ${SRC1}-${TGT},${SRC2}-${TGT} --source-lang ${SRC1} --target-lang ${TGT} --task factored_translation \
+        --output-file $OUTPUT_DIR/$OUTPUT --remove-bpe --multiple-encoders False
+
+
+# Factored ru DEPS
+
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints23-ru-deps"
+OUTPUT="extracted-attn-enru-f-deps.pkl"
+SRC1="en_tokensS"
+SRC2="en_deps"
+TGT="ru_tokensS"
+
+cuda_visible_devices="" stdbuf -i0 -e0 -o0 python $FAIRSEQ_DIR/encode_attention.py $DEST_DIR --path $CP_DIR/$CP --n-points $N \
+         --batch-size 1 --lang-pairs ${SRC1}-${TGT},${SRC2}-${TGT} --source-lang ${SRC1} --target-lang ${TGT} --task factored_translation \
+        --output-file $OUTPUT_DIR/$OUTPUT --remove-bpe --multiple-encoders False
+
+
+# Factored ru Lemmas
+
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints24-ru-lemmas"
+OUTPUT="extracted-attn-enru-f-lemmas.pkl"
+SRC1="en_tokensS"
+SRC2="en_lemmas"
+TGT="ru_tokensS"
+
+cuda_visible_devices="" stdbuf -i0 -e0 -o0 python $FAIRSEQ_DIR/encode_attention.py $DEST_DIR --path $CP_DIR/$CP --n-points $N \
+         --batch-size 1 --lang-pairs ${SRC1}-${TGT},${SRC2}-${TGT} --source-lang ${SRC1} --target-lang ${TGT} --task factored_translation \
+        --output-file $OUTPUT_DIR/$OUTPUT --remove-bpe --multiple-encoders False
+
+
+# Factored ru Syn-Lemmas
+
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints40-ru-syn"
+OUTPUT="extracted-attn-enru-f-syn-lemmas.pkl"
+SRC1="en_tokensS"
+SRC2="en_synsets_wo_at_lemmas"
+TGT="ru_tokensS"
+
+cuda_visible_devices="" stdbuf -i0 -e0 -o0 python $FAIRSEQ_DIR/encode_attention.py $DEST_DIR --path $CP_DIR/$CP --n-points $N \
+         --batch-size 1 --lang-pairs ${SRC1}-${TGT},${SRC2}-${TGT} --source-lang ${SRC1} --target-lang ${TGT} --task factored_translation \
+        --output-file $OUTPUT_DIR/$OUTPUT --remove-bpe --multiple-encoders False
+
+
+# Factored ru Syn-Pos
+
+CP_DIR="/home/usuaris/veu/ksenia.kharitonova/tfm/log/checkpoints41-ru-syn-pos"
+OUTPUT="extracted-attn-enru-f-syn-pos.pkl"
+SRC1="en_tokensS"
+SRC2="en_synsets_wo_at_pos"
+TGT="ru_tokensS"
+
+cuda_visible_devices="" stdbuf -i0 -e0 -o0 python $FAIRSEQ_DIR/encode_attention.py $DEST_DIR --path $CP_DIR/$CP --n-points $N \
+         --batch-size 1 --lang-pairs ${SRC1}-${TGT},${SRC2}-${TGT} --source-lang ${SRC1} --target-lang ${TGT} --task factored_translation \
+        --output-file $OUTPUT_DIR/$OUTPUT --remove-bpe --multiple-encoders False
+
+
