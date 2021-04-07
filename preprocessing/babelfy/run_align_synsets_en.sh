31b2d3d564ea9d6e1dfeee7e9310f00ed199699b
1254215 <ksenia.kharitonova@est.fib.upc.edu>
2021-04-07 11:22:56 +0200

    assign synsets mt fr for attn

--- preprocessing/babelfy/run_align_synsets_en.sh
+++ preprocessing/babelfy/run_align_synsets_en.sh
@@ -5,13 +5,15 @@
 #SBATCH --mem=30G # Memory
 #SBATCH --gres=gpu:1
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/mt_assign_synsets_pos_en_ru_new.log
+#SBATCH --output=/home/usuaris/veu/ksenia.kharitonova/tfm/log/mt_assign_synsets_all_en_fr_attn.log
 
-SCRIPT_PATH="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/preprocessing/babelfy/assign_align_synsets_without_at_pos_mt.py"
+SCRIPT_PATH1="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/preprocessing/babelfy/assign_align_synsets_without_at_lemmas_mt.py"
+SCRIPT_PATH2="/home/usuaris/veu/ksenia.kharitonova/tfm/src/fairseq-factored/preprocessing/babelfy/assign_align_synsets_without_at_pos_mt.py"
 PYTHON="python"
 
 source ~/.bashrc
 conda activate myenv
 
-stdbuf -i0 -e0 -o0 $PYTHON $SCRIPT_PATH
-#$PYTHON SSCRIPT_PATH > "/home/usuaris/veu/jordi.armengol/tfg/new/logs/get_synsets_de_1.log"
+stdbuf -i0 -e0 -o0 $PYTHON $SCRIPT_PATH1
+
+stdbuf -i0 -e0 -o0 $PYTHON $SCRIPT_PATH2
